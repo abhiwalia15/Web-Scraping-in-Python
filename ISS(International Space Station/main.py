6442565b80c8e9df89c52eb81f7dff10412555f8
Mrinal <waliamrinal@gmail.com>
2018-10-10 13:53:45 +0530

    last second commit

--- ISS(International Space Station/main.py
+++ ISS(International Space Station/main.py
@@ -12,8 +12,27 @@ response = requests.get('http://api.open-notify.org/iss-pass.json', params = par
 #This gets the same data as the command above
 #response = requests.get('http://api.open-notify.org/iss-pass.json?lat=40.71&lon=-74')
 
-
+#get the response data as a python object
+data = response.json()
+print(type(data))
+print(data)
+print('---------------------------------------------')
 
 #Print the content of the response (the data the server returned)
 print(response.status_code)
 print(response.content)
+print('------------------------')
+
+# Headers is a dictionary
+print(response.headers)
+
+# Get the content-type from the dictionary.
+print(response.headers["content-type"])
+print('---------------')
+
+#get the response from API endpoint
+response = requests.get('http://api.open-notify.org/astros.json')
+astros = response.json()
+
+print(astros)
+print(astros['number'])
